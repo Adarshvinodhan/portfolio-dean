0d0083b565b55b89f6cc712c2297bc9207bdaaaf
adarsh <adarshvinodhan@gmail.com>
2025-02-10 10:22:09 +0530

    production base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api'
+  baseURL: 'https://portfolio-dean-nodejs.onrender.com/api'
 });
 
 export default api;
