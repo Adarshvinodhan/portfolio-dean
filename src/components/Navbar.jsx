7d67411c6501d2fc168da1b61233985071c08619
adarsh <adarshvinodhan@gmail.com>
2025-02-11 17:14:07 +0530

    home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
-            <span className="font-bold text-xl">Dr. R. Saravana Moorthy</span>
+            <Link to="/" className="font-bold text-xl">Dr. R. Saravana Moorthy</Link>
           </div>
 
           {/* Desktop Nav */}
